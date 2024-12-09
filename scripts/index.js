21a04b03bf982e892c002c7be2c92006ba472203
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-12-10 00:25:38 +0200

    feat: handle cart functionality (open - close)

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 import "../sass/main.scss";
+
+// selection elements
+const cartIcon = document.querySelector(".cart__icon");
+const closeBtn = document.querySelector(".close");
+let body = document.querySelector("body");
+
+// event listeners
+cartIcon.addEventListener("click", toggleCart);
+closeBtn.addEventListener("click", closeCart);
+
+//functions
+function toggleCart() {
+  body.classList.toggle("active__tap-cart");
+}
+function closeCart() {
+  body.classList.remove("active__tap-cart");
+}
