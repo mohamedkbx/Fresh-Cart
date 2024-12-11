29d9412d9738f683648b65d8f7aeae641c3731ca
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-12-11 17:32:44 +0200

    feat: webpack config for Production

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,21 +5,34 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  entry: "./scripts/index.js",
+  entry: {
+    index: "./scripts/index.js",
+    login: "./scripts/login.js",
+  },
   mode: "production",
   optimization: {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           compress: {
-            drop_console: true, // Remove console.log statements
+            drop_console: true,
           },
           output: {
-            comments: false, // Remove comments from output
+            comments: false,
           },
         },
       }),
     ],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    port: 5000,
+    hot: true,
+    historyApiFallback: true, // Enable single-page application routing
+    open: true, // Automatically opens the browser
+    historyApiFallback: {
+      index: "./pages/login.html",
+    },
   },
   module: {
     rules: [
@@ -46,6 +59,11 @@ module.exports = {
     ],
   },
 
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
@@ -56,6 +74,12 @@ module.exports = {
       template: "./template.html",
       filename: "template.html",
       favicon: "./assets/favicon.ico",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./pages/login.html",
+      filename: "login.html",
+      favicon: "./assets/favicon.ico",
+      chunks: ["login"],
     }),
 
     new MiniCssExtractPlugin({
